435293ccc34a7f06d3c35db349fa2083aa978b7f
Sonia Suárez <ssuarezcode@outlook.com>
2024-06-27 11:49:26 -0500

    Adding new classes.

---
+++ src/Decisiones.java
@@ -0,0 +1,20 @@
+public class Decisiones {
+    public static void main(String[] args) {
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+        String tipoPlan = "Plus";
+
+//        if (fechaDeLanzamiento > 2022){
+//            System.out.println("Películas más populares");
+//        }else {
+//            System.out.println("Películas retro que aún vale la pena ver");
+//        }
+
+        if (incluidoEnElPlan && tipoPlan.equals("Plus")){
+            System.out.println("Disfrute de su película");
+        }else {
+            System.out.println("Película no disponible para su plan actual");
+        }
+    }
+}
---
+++ src/Lectura.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Lectura {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Escribe el nombre de tu película favorita: ");
+        String pelicula = teclado.nextLine();
+        System.out.println("Ahora escribe la fecha de lanzamiento: ");
+        int fechaDeLanzamiento = teclado.nextInt();
+        System.out.println("Por último dinos qué nota le das a la película: ");
+        double nota = teclado.nextDouble();
+
+        System.out.println(pelicula);
+        System.out.println(fechaDeLanzamiento);
+        System.out.println(nota);
+    }
+}
